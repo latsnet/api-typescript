c8cbccc98f1da014482c48fc38bf7b1e78d07eb0
Luiz Augusto Tavares da Silveira <luiz.silveira@me.com.br>
2020-06-09 19:19:00 -0300

    Project of API using Node, Typescript and MongoDb

---
+++ controller/newsController.ts
@@ -0,0 +1,55 @@
+import NewsService from '../services/newsService';
+import Helper from '../infra/helper';
+
+import * as HttpStatus from 'http-status';
+
+class NewsController {
+
+
+    get(req, res) {
+
+        NewsService.get()
+            .then(news => Helper.sendResponse(res, HttpStatus.OK, news))
+            .catch(error => console.error.bind(console, `Error ${error}`));
+    }
+
+    getById(req, res) {
+
+        const _id = req.params.id;
+
+        NewsService.getById(_id)
+            .then(news => Helper.sendResponse(res, HttpStatus.OK, news))
+            .catch(error => console.error.bind(console, `Error ${error}`));
+    }
+
+    create(req, res) {
+
+        let vm = req.body;
+        console.log(vm);
+
+        NewsService.create(vm)
+            .then(news => Helper.sendResponse(res, HttpStatus.OK, "Notícia cadastrada com sucesso"))
+            .catch(error => console.error.bind(console, `Error ${error}`));
+    }
+
+    update(req, res) {
+
+        const _id = req.params.id;
+        let news = req.body;
+
+        NewsService.update(_id, news)
+            .then(news => Helper.sendResponse(res, HttpStatus.OK, 'Notícia atualizada com sucesso'))
+            .catch(error => console.error.bind(console, `Error ${error}`));
+    }
+
+    delete(req, res) {
+        const _id = req.params.id;
+
+        NewsService.delete(_id)
+            .then(() => Helper.sendResponse(res, HttpStatus.OK, "Notícia excluída com sucesso"))
+            .catch(error => console.error.bind(console, `Error ${error}`));
+    }
+
+}
+
+export default new NewsController();
